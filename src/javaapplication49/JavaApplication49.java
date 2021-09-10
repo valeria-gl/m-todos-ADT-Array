c6527fdce2657d612491dd1cdfc6fc2bf9e1c3c0
valeria-gl <78509379+valeria-gl@users.noreply.github.com>
2021-09-09 22:59:12 -0500

    Tarea

---
+++ src/javaapplication49/JavaApplication49.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication49;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JavaApplication49 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int array[] = {1, 2, 3 ,4};
+        
+
+        int numeroDeElementos = array.length;
+        System.out.println(numeroDeElementos);
+        
+        
+    }
+    
+}
